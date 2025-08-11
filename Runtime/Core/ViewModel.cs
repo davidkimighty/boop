b4fc3c0d973d4c09f63c9cc2828caca57d8e2d56
davidkimighty <davidkimighty@gmail.com>
2025-08-11 17:09:30 +0900

    adding generic version

--- Runtime/Core/ViewModel.cs
+++ Runtime/Core/ViewModel.cs
@@ -5,17 +5,33 @@ namespace boop
 {
     public abstract class ViewModel : IViewModel, IDisposable
     {
+        public event Action OnDataChange;
+
         public virtual void Dispose() { }
 
-        protected bool SetProperty<T>(ref T field, T value, Action actionEvent = null)
+        protected bool SetProperty<T>(ref T field, T value, Action propertyEvent = null)
         {
             if (EqualityComparer<T>.Default.Equals(field, value)) return false;
 
             field = value;
-            actionEvent?.Invoke();
+            propertyEvent?.Invoke();
+            OnDataChange?.Invoke();
+            return true;
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, Action<T> propertyEvent = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+            field = value;
+            propertyEvent?.Invoke(value);
+            OnDataChange?.Invoke();
             return true;
         }
     }
 
-    public interface IViewModel { }
+    public interface IViewModel
+    {
+        event Action OnDataChange;
+    }
 }
